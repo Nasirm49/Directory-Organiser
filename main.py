629e7de7b16ef6def2fbb4c953d6c3ea7f49b427
Nasir <nasirmohamed49@outlook.com>
2023-12-30 21:38:07 +0000

    Added conditions for: * image files pdf and .exe files * written code to move files.

--- main.py
+++ main.py
@@ -9,5 +9,16 @@ downloads_path= "C:\\Users\\nasir\\Downloads\\"
 files = os.listdir(downloads_path)
 
 for file in files:
-    if ".jpg" in file or ".png" in file:
-        shutil.move(f"{downloads_path}{file}", f"{path}Pictures") 
+    try:
+        if ".jpg" in file or ".jpeg" in file or ".png" in file:
+            shutil.move(f"{downloads_path}{file}", f"{path}OneDrive\\Pictures") 
+        elif ".pdf" in file or ".docx" in file or ".doc" in file:
+            shutil.move(f"{downloads_path}{file}", f"{path}OneDrive\\Documents") 
+        elif ".iso" in file: 
+            shutil.move(f"{downloads_path}{file}", f"{path}OneDrive\\IsoFiles")    
+        elif ".exe" in file:
+            shutil.move(f"{downloads_path}{file}", f"{path}OneDrive\\myPrograms")
+                           
+    except shutil.Error:
+        print(f"Error!\n shutil error: {file} already exists")
+      
